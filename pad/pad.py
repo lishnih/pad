8cee5695353bf8dddf61d186997147a24ec5a573
Stan <lishnih@gmail.com>
2013-08-22 13:25:04 +0400

    Function to open a file added.

--- pad/pad.py
+++ pad/pad.py
@@ -5,7 +5,7 @@
 from __future__ import ( division, absolute_import,
                          print_function, unicode_literals )
 
-import sys, os, re, logging
+import sys, os, re, codecs, logging
 
 
 __pkgname__ = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
@@ -32,21 +32,20 @@ else:
 class ScrolledText(tk.Frame):
     def __init__(self, parent=None):
         tk.Frame.__init__(self, parent)
-        self.pack(fill=tk.BOTH, expand=tk.YES)
 
         self.text = tk.Text(self, relief=tk.SUNKEN)
-        sbar = tk.Scrollbar(self)
-        sbar.config(command=self.text.yview)
-        self.text.config(yscrollcommand=sbar.set)
-
         self.text.pack(fill=tk.BOTH, side=tk.LEFT, expand=tk.YES)
-        sbar.pack(fill=tk.Y, side=tk.RIGHT)
 
+        sbar = tk.Scrollbar(self)
+        sbar.pack(fill=tk.Y, side=tk.RIGHT)
+        sbar.config(command=self.text.yview)
+
+        self.text.config(yscrollcommand=sbar.set)
         self.text.config(font=('Courier', 9, 'normal'))
 
     def appendText(self, text=""):
         self.text.insert(tk.INSERT, text)
-        self.text.mark_set(INSERT, '1.0')
+        self.text.mark_set(tk.INSERT, '1.0')
         self.text.focus()
 
     def setText(self, text=""):
@@ -58,6 +57,18 @@ class ScrolledText(tk.Frame):
 
     def bind(self, event, handler, add=None):
         self.text.bind(event, handler, add)
+
+
+class Statusbar(tk.Frame):
+    def __init__(self, parent=None):
+        tk.Frame.__init__(self, parent)
+
+        self.status = tk.StringVar()
+        self.label = tk.Label(self, textvariable=self.status, anchor=tk.W)
+        self.label.pack(fill=tk.X)
+
+    def setText(self, text=""):
+        self.status.set(text)
 
 
 class Menu(tk.Menu):
@@ -89,6 +100,11 @@ class AppUI(tk.Tk):
         tk.Tk.__init__(self)
         self.title("tkPad")
 
+        ### Vars ###
+
+        self.filename = None
+        self.filetype = None
+
         ### Menu ###
 
         self.menubar = tk.Menu(self)
@@ -96,75 +112,74 @@ class AppUI(tk.Tk):
         menu = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="File", menu=menu, underline=0)
         menu.add_command(command=self.onFileNew, label="New", accelerator="Ctrl+N", underline=0)
-        menu.add_command(command=self.onFileLoad, label="Load", accelerator="Ctrl+O")
-        menu.add_command(command=self.onFileClose, label="Close", accelerator="Ctrl+F4")
+        menu.add_command(command=self.onFileLoad, label="Open", accelerator="Ctrl+O", underline=0)
+        menu.add_command(command=self.onFileClose, label="Close", accelerator="Ctrl+F4", underline=0)
         menu.add_separator()
-        menu.add_command(command=self.onFileSave, label="Save", accelerator="Ctrl+S")
+        menu.add_command(command=self.onFileSave, label="Save", accelerator="Ctrl+S", underline=0)
         menu.add_command(command=self.onFileSaveAs, label="Save As...")
         menu.add_separator()
-        menu.add_command(command=self.onFileInfo, label="Info")
+        menu.add_command(command=self.onFileInfo, label="Info", underline=0)
         menu.add_separator()
-        menu.add_command(command=self.onFilePrint, label="Print", accelerator="Ctrl+P")
+        menu.add_command(command=self.onFilePrint, label="Print", accelerator="Ctrl+P", underline=0)
         menu.add_command(command=self.onFilePrintSettings, label="Print settings", accelerator="Alt+P")
-        menu.add_command(command=self.onFilePreview, label="Preview")
+        menu.add_command(command=self.onFilePreview, label="Preview", underline=3)
         menu.add_separator()
-        menu.add_command(command=self.onFileExit, label="Exit", accelerator="Ctrl+Q")
+        menu.add_command(command=self.onFileExit, label="Exit", accelerator="Ctrl+Q", underline=1)
 
-        menu = tk.Menu(self.menubar, tearoff=0)
+        menu = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="Edit", menu=menu, underline=0)
-        menu.add_command(command=self.onEditUndo, label="Undo")
-        menu.add_command(command=self.onEditRedo, label="Redo")
+        menu.add_command(command=self.onEditUndo, label="Undo", accelerator="Ctrl+Z", underline=0)
+        menu.add_command(command=self.onEditRedo, label="Redo", accelerator="Ctrl+Shift-Z", underline=0)
         menu.add_separator()
-        menu.add_command(command=self.onEditCut, label="Cut")
-        menu.add_command(command=self.onEditCopy, label="Copy")
-        menu.add_command(command=self.onEditPaste, label="Paste")
-        menu.add_command(command=self.onEditDelete, label="Delete")
+        menu.add_command(command=self.onEditCut, label="Cut", accelerator="Ctrl+X", underline=1)
+        menu.add_command(command=self.onEditCopy, label="Copy", accelerator="Ctrl+C", underline=0)
+        menu.add_command(command=self.onEditPaste, label="Paste", accelerator="Ctrl+V", underline=0)
+        menu.add_command(command=self.onEditDelete, label="Delete", underline=0)
         menu.add_separator()
-        menu.add_command(command=self.onEditSelectAll, label="Select all")
+        menu.add_command(command=self.onEditSelectAll, label="Select all", accelerator="Ctrl+A", underline=7)
 
-        menu = tk.Menu(self.menubar, tearoff=0)
+        menu = Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="Help", menu=menu, underline=0)
-        menu.add_command(command=self.onHelpAbout, label="About")
+        menu.add_command(command=self.onHelpAbout, label="About", underline=0)
 
         self.config(menu=self.menubar)
 
         ### Widgets ###
 
         # Text Widget
-        self.text1 = ScrolledText()
+        self.text = ScrolledText(self)
+        self.text.pack(fill=tk.BOTH, expand=tk.YES)
 
         # Status Widget
-        self.status = tk.StringVar()
-        label1 = tk.Label(self, textvariable=self.status, anchor=tk.W)
-
-        ### Grid widgets ###
-
-        self.text1.pack(fill=tk.BOTH, expand=1)
-        label1.pack(fill=tk.X)
+        self.status = Statusbar(self)
+        self.status.pack(fill=tk.X)
 
         ### Bind ###
 
         self.protocol('WM_DELETE_WINDOW', self.onFileExit)
-        self.text1.bind("<Button-3>", self.onRButton)
+        self.text.bind("<Button-3>", self.onRButton)
 
         ### Initial ###
 
         self.update_idletasks()
         self.minsize(self.winfo_reqwidth(), self.winfo_reqheight())
 
-    def setStatus(self, text=""):
-        self.status.set(text)
-
     ### Events ###
 
     def onFileNew(self, event=None):
-        print("New")
+        self.filename = "New.txt"
+        self.filetype = None
+        self.text.setText()
+        self.status.setText(self.filename)
 
     def onFileLoad(self, event=None):
-        pass
+        self.loadfileDialog()
 
     def onFileClose(self, event=None):
-        pass
+        self.filename = None
+        self.filetype = None
+        self.text.setText()
+        self.status.setText(self.filename)
 
     def onFileSave(self, event=None):
         pass
@@ -221,12 +236,33 @@ Package: {4}
            sys.version, __package__)
         showinfo("About", text)
 
+    ### Dialogs ###
+
+    def loadfileDialog(self):
+        filename = askopenfilename(filetypes=[
+                       ('All files', '*.*'),
+                   ])
+        if filename:
+            self.loadfile(filename)
+
+    ### Functions ###
+
+    def loadfile(self, filename):
+        if os.path.isfile(filename):
+            with codecs.open(filename) as f:
+                text = f.read()
+                self.text.setText(text)
+
+        self.filename = filename
+        self.filetype = None
+        self.status.setText(self.filename)
+
 
 
 def main(args):
     root = AppUI()
     for i in args.files:
-        print(i)
+        root.loadfile(i)
     root.mainloop()
 
 
