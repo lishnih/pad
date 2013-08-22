ed1219ddb058890f0703239308aa6a1f634c5b55
Stan <lishnih@gmail.com>
2013-08-22 10:36:04 +0400

    Append class Menu for automatic processing accelerators in add_command.

--- pad/pad.py
+++ pad/pad.py
@@ -5,7 +5,7 @@
 from __future__ import ( division, absolute_import,
                          print_function, unicode_literals )
 
-import sys, os, logging
+import sys, os, re, logging
 
 
 __pkgname__ = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
@@ -31,33 +31,57 @@ else:
 
 class ScrolledText(tk.Frame):
     def __init__(self, parent=None):
-        tk.Frame.__init__(self, parent)             
+        tk.Frame.__init__(self, parent)
         self.pack(fill=tk.BOTH, expand=tk.YES)
 
         self.text = tk.Text(self, relief=tk.SUNKEN)
         sbar = tk.Scrollbar(self)
-        sbar.config(command=self.text.yview)                  
-        self.text.config(yscrollcommand=sbar.set)           
+        sbar.config(command=self.text.yview)
+        self.text.config(yscrollcommand=sbar.set)
 
-        sbar.pack(fill=tk.Y, side=tk.RIGHT)                   
-        self.text.pack(fill=tk.BOTH, side=tk.LEFT, expand=tk.YES)     
+        self.text.pack(fill=tk.BOTH, side=tk.LEFT, expand=tk.YES)
+        sbar.pack(fill=tk.Y, side=tk.RIGHT)
 
-        self.text.config(font=('courier', 9, 'normal'))
+        self.text.config(font=('Courier', 9, 'normal'))
 
     def appendText(self, text=""):
         self.text.insert(tk.INSERT, text)
-        self.text.mark_set(INSERT, '1.0')              
-        self.text.focus()                                
+        self.text.mark_set(INSERT, '1.0')
+        self.text.focus()
 
     def setText(self, text=""):
         self.text.delete(1.0, tk.END)
         self.appendText(text)
 
-    def getText(self):                               
-        return self.text.get('1.0', tk.END+'-1c')         
+    def getText(self):
+        return self.text.get('1.0', tk.END+'-1c')
 
-    def bind(self, event, handler, add=None):                               
+    def bind(self, event, handler, add=None):
         self.text.bind(event, handler, add)
+
+
+class Menu(tk.Menu):
+    def __init__(self, master=None, **options):
+        tk.Menu.__init__(self, master, **options)
+
+    def add_command(self, **options):
+        tk.Menu.add_command(self, **options)
+
+        accelerator = options.get('accelerator')
+        command = options.get('command')
+        if accelerator:
+            res = re.split('[+-]', accelerator)
+            # Control, Ctrl, Option, Opt, Alt, Shift, Command, Cmd, Meta
+            modifiers = [i.replace('Ctrl', 'Control').replace('Opt', 'Option').\
+                         replace('Cmd', 'Command') for i in res[:-1]]
+            modifiers = '-'.join(modifiers)
+            key = res[-1]
+            # Control, Option, Alt, Shift, Command, Meta
+            if len(key) == 1:
+                self.bind_all("<{0}-{1}>".format(modifiers, key.upper()), command)
+                self.bind_all("<{0}-{1}>".format(modifiers, key.lower()), command)
+            else:
+                self.bind_all("<{0}-{1}>".format(modifiers, key), command)
 
 
 class AppUI(tk.Tk):
@@ -69,25 +93,25 @@ class AppUI(tk.Tk):
 
         self.menubar = tk.Menu(self)
 
-        menu = tk.Menu(self.menubar, tearoff=0)
-        self.menubar.add_cascade(label="File", menu=menu)
-        menu.add_command(command=self.onFileNew, label="New")
-        menu.add_command(command=self.onFileLoad, label="Load")
-        menu.add_command(command=self.onFileClose, label="Close")
+        menu = Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label="File", menu=menu, underline=0)
+        menu.add_command(command=self.onFileNew, label="New", accelerator="Ctrl+N", underline=0)
+        menu.add_command(command=self.onFileLoad, label="Load", accelerator="Ctrl+O")
+        menu.add_command(command=self.onFileClose, label="Close", accelerator="Ctrl+F4")
         menu.add_separator()
-        menu.add_command(command=self.onFileSave, label="Save")
+        menu.add_command(command=self.onFileSave, label="Save", accelerator="Ctrl+S")
         menu.add_command(command=self.onFileSaveAs, label="Save As...")
         menu.add_separator()
         menu.add_command(command=self.onFileInfo, label="Info")
         menu.add_separator()
-        menu.add_command(command=self.onFilePrint, label="Print")
-        menu.add_command(command=self.onFilePrintSettings, label="Print settings")
+        menu.add_command(command=self.onFilePrint, label="Print", accelerator="Ctrl+P")
+        menu.add_command(command=self.onFilePrintSettings, label="Print settings", accelerator="Alt+P")
         menu.add_command(command=self.onFilePreview, label="Preview")
         menu.add_separator()
-        menu.add_command(command=self.onFileExit, label="Exit")
+        menu.add_command(command=self.onFileExit, label="Exit", accelerator="Ctrl+Q")
 
         menu = tk.Menu(self.menubar, tearoff=0)
-        self.menubar.add_cascade(label="Edit", menu=menu)
+        self.menubar.add_cascade(label="Edit", menu=menu, underline=0)
         menu.add_command(command=self.onEditUndo, label="Undo")
         menu.add_command(command=self.onEditRedo, label="Redo")
         menu.add_separator()
@@ -99,7 +123,7 @@ class AppUI(tk.Tk):
         menu.add_command(command=self.onEditSelectAll, label="Select all")
 
         menu = tk.Menu(self.menubar, tearoff=0)
-        self.menubar.add_cascade(label="Help", menu=menu)
+        self.menubar.add_cascade(label="Help", menu=menu, underline=0)
         menu.add_command(command=self.onHelpAbout, label="About")
 
         self.config(menu=self.menubar)
@@ -117,7 +141,7 @@ class AppUI(tk.Tk):
 
         self.text1.pack(fill=tk.BOTH, expand=1)
         label1.pack(fill=tk.X)
-    
+
         ### Bind ###
 
         self.protocol('WM_DELETE_WINDOW', self.onFileExit)
@@ -133,63 +157,63 @@ class AppUI(tk.Tk):
 
     ### Events ###
 
-    def onFileNew(self):
+    def onFileNew(self, event=None):
+        print("New")
+
+    def onFileLoad(self, event=None):
         pass
 
-    def onFileLoad(self):
+    def onFileClose(self, event=None):
         pass
 
-    def onFileClose(self):
+    def onFileSave(self, event=None):
         pass
 
-    def onFileSave(self):
+    def onFileSaveAs(self, event=None):
         pass
 
-    def onFileSaveAs(self):
+    def onFileInfo(self, event=None):
         pass
 
-    def onFileInfo(self):
+    def onFilePrint(self, event=None):
         pass
 
-    def onFilePrint(self):
+    def onFilePrintSettings(self, event=None):
         pass
 
-    def onFilePrintSettings(self):
+    def onFilePreview(self, event=None):
         pass
 
-    def onFilePreview(self):
-        pass
-
-    def onFileExit(self):
+    def onFileExit(self, event=None):
         res = askokcancel("Exit", "Really quit?")
         if res:
             self.destroy()
 
-    def onEditUndo(self):
+    def onEditUndo(self, event=None):
         pass
 
-    def onEditRedo(self):
+    def onEditRedo(self, event=None):
         pass
 
-    def onEditCut(self):
+    def onEditCut(self, event=None):
         pass
 
-    def onEditCopy(self):
+    def onEditCopy(self, event=None):
         pass
 
-    def onEditPaste(self):
+    def onEditPaste(self, event=None):
         pass
 
-    def onEditDelete(self):
+    def onEditDelete(self, event=None):
         pass
 
-    def onEditSelectAll(self):
+    def onEditSelectAll(self, event=None):
         pass
 
-    def onRButton(self, event):
+    def onRButton(self, event=None):
         print("Right")
 
-    def onHelpAbout(self):
+    def onHelpAbout(self, event=None):
         text = """{0}\n{1}\nVersion {2}\n
 Python: {3}
 Package: {4}
